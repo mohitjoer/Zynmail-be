f2acb5b8e20eaed08598aad9b4bb38e7b07aa502
mohitjoer <mohitjoe.r@gmail.com>
2025-12-28 22:21:52 +0530

    commit	new file:   .github/COPILOT_INSTRUCTIONS.md commit	new file:   src/config/settings.py

---
+++ src/config/settings.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings:
+    """Application settings"""
+    #FRONTEND_ROUTE= os.getenv("FRONTEND_ROUTE")
+    #PASS_KEY = os.getenv("PASS_KEY")
+    
+
+settings = Settings()
